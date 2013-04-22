cf0f6d855b414bdc672536118117fbf938e84b92
gabac <gabathuler@gmail.com>
2013-04-22 13:57:56 +0200

    Added HostViewController only for the iPad

---
+++ iPlanningPoker/HostViewController.m
@@ -0,0 +1,38 @@
+//
+//  HostViewController.m
+//  iPlanningPoker
+//
+//  Created by Cyril Gabathuler on 22.04.13.
+//  Copyright (c) 2013 Cyril Gabathuler. All rights reserved.
+//
+
+#import "HostViewController.h"
+
+@interface HostViewController ()
+
+@end
+
+@implementation HostViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
