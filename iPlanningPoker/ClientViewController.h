77dae24e4d7cca8719292d747e30110fc69ea27f
gabac <gabathuler@gmail.com>
2013-04-22 17:13:31 +0200

    ClientViewController

---
+++ iPlanningPoker/ClientViewController.h
@@ -0,0 +1,13 @@
+//
+//  ClientViewController.h
+//  iPlanningPoker
+//
+//  Created by Cyril Gabathuler on 22.04.13.
+//  Copyright (c) 2013 Cyril Gabathuler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClientViewController : UIViewController
+
+@end
---
+++ iPlanningPoker/ClientViewController.m
@@ -0,0 +1,38 @@
+//
+//  ClientViewController.m
+//  iPlanningPoker
+//
+//  Created by Cyril Gabathuler on 22.04.13.
+//  Copyright (c) 2013 Cyril Gabathuler. All rights reserved.
+//
+
+#import "ClientViewController.h"
+
+@interface ClientViewController ()
+
+@end
+
+@implementation ClientViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
