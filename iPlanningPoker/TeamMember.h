f35ed8e09e6013725ccad3357dc507c211d906fc
gabac <gabathuler@gmail.com>
2013-11-06 10:05:20 +0100

    finished signin

--- iPlanningPoker/TeamMember.h
+++ iPlanningPoker/TeamMember.h
@@ -10,4 +10,8 @@
 
 @interface TeamMember : NSObject
 
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *peerID;
+@property (nonatomic, assign) BOOL receivedResponse;
+
 @end
--- iPlanningPoker/TeamMember.m
+++ iPlanningPoker/TeamMember.m
@@ -10,4 +10,5 @@
 
 @implementation TeamMember
 
+
 @end
